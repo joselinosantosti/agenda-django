35901e4786ad4b9faa87a148cfa664c695fe239d
joselinosantosti <joselinosantosti@gmail.com>
2022-03-11 20:54:30 -0300

    Simple pagination Close #17

--- contatos/views.py
+++ contatos/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from.models import Contato
 from django.http import Http404
+from django.core.paginator import Paginator
 
 def index(request):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 10)
+    page = request.GET.get('p')
+    total = request.GET.get('total')
+    contatos = paginator.get_page(page)
     
     return render(request, 'contatos/index.html', {
         'contatos':contatos
