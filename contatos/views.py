326710f81ccab619afbc494f3d08a4929e589ed8
joselinosantosti <joselinosantosti@gmail.com>
2022-03-13 11:37:00 -0300

    Search field Close #19

--- contatos/views.py
+++ contatos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from.models import Contato
 from django.http import Http404
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 def index(request):
     contatos = Contato.objects.order_by('id').filter(mostrar=True)
@@ -20,4 +22,23 @@ def ver_contato(request, contato_id):
         raise Http404
     return render(request, 'contatos/contato.html', {
         'contato':contato
+    })
+
+def busca(request):
+    busca = request.GET.get('busca')
+    
+    if busca is None or not busca:
+        raise Http404()
+
+    campos = Concat('nome', Value(' '), 'sobrenome')
+
+    contatos = Contato.objects.annotate(
+        nome_completo = campos
+    ).order_by('id').filter(Q(nome_completo__icontains = busca) | Q(telefone__icontains = busca), mostrar=True)
+    paginator = Paginator(contatos, 5)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    
+    return render(request, 'contatos/busca.html', {
+        'contatos':contatos
     })
